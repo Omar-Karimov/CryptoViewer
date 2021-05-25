cab69cc2ccdaaaa67ea1a9bd969f776e55eb5dc8
Omar <68358028+Omar-Karimov@users.noreply.github.com>
2021-05-25 17:40:50 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 
 import pandas as pd
 import tweepy
@@ -6,7 +7,6 @@ from wordcloud import WordCloud
 import plotly.graph_objs as go
 import os
 import re
-from __future__ import print_function
 import pystan
 import numpy as np
 import streamlit as st
